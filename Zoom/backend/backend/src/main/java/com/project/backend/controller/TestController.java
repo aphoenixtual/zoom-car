afad82b45db7aa30f3e61905b1c9fc8067f78f26
prashant979 <vishwakarmaprashant37@gmail.com>
2021-12-03 08:20:21 -0800

    Starter files

---
+++ Zoom/backend/backend/src/main/java/com/project/backend/controller/TestController.java
@@ -0,0 +1,20 @@
+package com.project.backend.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("/home")
+public class TestController {
+
+	RestTemplate template = new RestTemplate();
+
+	@GetMapping("/")
+	public String check() {
+		String url = "http://localhost:8080/test/";
+		return template.getForObject(url, String.class);
+	}
+}
