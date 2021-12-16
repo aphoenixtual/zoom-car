bb9320481067f38bdcb96555dd8f44e5e22aa44f
Aayush Patel <ayushp264@gmail.com>
2021-12-16 16:39:44 +0530

    Delete client directory

--- client/src/api/colorService.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-
-export default class ColorService{
-    getAllColors(){
-        console.log("getAllColors çağrıldı")
-        return axios.get("https://springrestapi-carrental.herokuapp.com/api/colors/getAll")
-    }
-}
